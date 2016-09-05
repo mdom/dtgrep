ccf3eb6561ac71eea7a3f876ec78d1e022899427
Mario Domgoergen <mdom@taz.de>
2016-09-05 12:16:13 +0200

    Test addYear

--- main_test.go
+++ main_test.go
@@ -10,9 +10,9 @@ type FillDateTest struct {
 	result   string
 }
 
-func TestFillDate(t *testing.T) {
+func TestAddYear(t *testing.T) {
 
-	now,_ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+	now, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
 
 	tests := []FillDateTest{
 		{"0000-01-02T15:04:05Z", "2006-01-02T15:04:05Z"},
@@ -20,9 +20,9 @@ func TestFillDate(t *testing.T) {
 	}
 
 	for _, v := range tests {
-		argument,_ := time.Parse(time.RFC3339,v.argument)
-		result,_ := time.Parse(time.RFC3339,v.result)
-		if fillDate(argument, now) != result {
+		argument, _ := time.Parse(time.RFC3339, v.argument)
+		result, _ := time.Parse(time.RFC3339, v.result)
+		if addYear(argument, now) != result {
 			t.Error("fillDate failed", argument, result)
 		}
 	}
